d98f97e076d364e8c5f0271abcf7c1f1472def86
Amrah2609 <emrah.kerimov1111@gmail.com>
2025-02-09 01:30:24 +0400

    Yeni fayllar əlavə olundu

---
+++ devhubtapsiriq6.py
@@ -0,0 +1,9 @@
+# Məqsəd: Sinifin (class) necə yaradılacağı, nümunələrin (instances) necə istifadə ediləcəyi və atributlara necə müraciət ediləcəyi barədə baza anlayışlarını öyrənmək.
+
+# Təlimatlar:
+
+# Person adlı sinif yaradın.
+# Bu sinifdə name və age atributları olsun.
+# __init__ metodu yazaraq bu atributları təyin edin.
+# Müxtəlif name və age dəyərləri ilə iki-üç Person obyekti yaradın.
+# Hər obyektin atributlarını ekranda (print vasitəsilə) göstərin.
